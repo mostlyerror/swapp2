1fb5860ac93155130908d54eb376a2bcfdd0d918
Benjamin Poon <benjamintpoon@gmail.com>
2022-03-05 15:57:07 -0700

    add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    root_path
+    # if current_user.has_role?(:admin)
+    #  dashboard_path
+    # elsif current_user.has_role?(:student)
+    #  root_path
+    # else
+    #  root_path
+    # end
+   end
 end
